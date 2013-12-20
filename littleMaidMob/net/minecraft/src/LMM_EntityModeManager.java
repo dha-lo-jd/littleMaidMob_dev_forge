562c583eab3a58be3f60f742b86e260f645ecf8d
dha_lo_jd <dha_1024_blame@hotmail.com>
2013-12-21 08:50:36 +0900

    メンバのソート

--- littleMaidMob/net/minecraft/src/LMM_EntityModeManager.java
+++ littleMaidMob/net/minecraft/src/LMM_EntityModeManager.java
@@ -8,19 +8,39 @@ public class LMM_EntityModeManager extends MMM_ManagerBase {
 	public static final String prefix = "EntityMode";
 	public static List<LMM_EntityModeBase> maidModeList = new ArrayList<LMM_EntityModeBase>();
 
+	/**
+	 * AI追加用のリストを獲得。
+	 */
+	public static List<LMM_EntityModeBase> getModeList(LMM_EntityLittleMaid pentity) {
+		List<LMM_EntityModeBase> llist = new ArrayList<LMM_EntityModeBase>();
+		for (LMM_EntityModeBase lmode : maidModeList) {
+			try {
+				llist.add(lmode.getClass().getConstructor(LMM_EntityLittleMaid.class).newInstance(pentity));
+			} catch (Exception e) {
+			} catch (Error e) {
+			}
+		}
+
+		return llist;
+	}
 
 	public static void init() {
 		// 特定名称をプリフィックスに持つmodファイをを獲得
 		MMM_FileManager.getModFile("EntityMode", prefix);
 	}
-	
+
 	public static void loadEntityMode() {
 		(new LMM_EntityModeManager()).load();
 	}
 
-	@Override
-	public String getPreFix() {
-		return prefix;
+	/**
+	 * ロードされているモードリストを表示する。
+	 */
+	public static void showLoadedModes() {
+		mod_LMM_littleMaidMob.Debug("Loaded Mode lists(%d)", maidModeList.size());
+		for (LMM_EntityModeBase lem : maidModeList) {
+			mod_LMM_littleMaidMob.Debug("%04d : %s", lem.priority(), lem.getClass().getSimpleName());
+		}
 	}
 
 	@Override
@@ -30,12 +50,12 @@ public class LMM_EntityModeManager extends MMM_ManagerBase {
 		if (!LMM_EntityModeBase.class.isAssignableFrom(pclass)) {
 			return false;
 		}
-		
+
 		try {
 			LMM_EntityModeBase lemb = null;
-			lemb = (LMM_EntityModeBase)pclass.getConstructor(LMM_EntityLittleMaid.class).newInstance((LMM_EntityLittleMaid)null);
+			lemb = (LMM_EntityModeBase) pclass.getConstructor(LMM_EntityLittleMaid.class).newInstance((LMM_EntityLittleMaid) null);
 			lemb.init();
-			
+
 			if (maidModeList.isEmpty() || lemb.priority() >= maidModeList.get(maidModeList.size() - 1).priority()) {
 				maidModeList.add(lemb);
 			} else {
@@ -55,30 +75,9 @@ public class LMM_EntityModeManager extends MMM_ManagerBase {
 		return false;
 	}
 
-	/**
-	 * AI追加用のリストを獲得。 
-	 */
-	public static List<LMM_EntityModeBase> getModeList(LMM_EntityLittleMaid pentity) {
-		List<LMM_EntityModeBase> llist = new ArrayList<LMM_EntityModeBase>();
-		for (LMM_EntityModeBase lmode : maidModeList) {
-			try {
-				llist.add(lmode.getClass().getConstructor(LMM_EntityLittleMaid.class).newInstance(pentity));
-			} catch (Exception e) {
-			} catch (Error e) {
-			}
-		}
-		
-		return llist;
-	}
-
-	/**
-	 * ロードされているモードリストを表示する。
-	 */
-	public static void showLoadedModes() {
-		mod_LMM_littleMaidMob.Debug("Loaded Mode lists(%d)", maidModeList.size());
-		for (LMM_EntityModeBase lem : maidModeList) {
-			mod_LMM_littleMaidMob.Debug("%04d : %s", lem.priority(), lem.getClass().getSimpleName());
-		}
+	@Override
+	public String getPreFix() {
+		return prefix;
 	}
 
 }
